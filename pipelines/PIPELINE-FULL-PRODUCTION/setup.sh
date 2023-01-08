52a53cdf815b48669718f367df7b7bb07d8faa94
Ubuntu <ubuntu@ip-172-31-6-221.ec2.internal>
2023-01-08 10:25:38 +0000

    cambios funcione el pipeline

--- pipelines/PIPELINE-FULL-PRODUCTION/setup.sh
+++ pipelines/PIPELINE-FULL-PRODUCTION/setup.sh
@@ -5,9 +5,7 @@ python3.7 -m venv todo-list-aws
 source todo-list-aws/bin/activate
 python -m pip install --upgrade pip
 python -m pip install awscli
-python -m pip install aws-sam-cli
+python -m pip install aws-sam-cli --force-reinstall "cryptography==38.0.4"
 # For integration testing
 python -m pip install pytest
-python -m pip install cryptography
-python -m pip install pyopenssl
 pwd
